31144a088378e93267a4c481da645d50309018c1
Mike Decker <pookmish@gmail.com>
2021-05-13 09:57:41 -0700

    fixed

---
+++ .gitpod/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+composer install -n
+vendor/bin/blt alias --no-interaction
+source ~/.bash_profile
+blt blt:telemetry:disable --no-interaction
+gp env APACHE_DOCROOT_IN_REPO=docroot
+eval $(gp env -e)
+cp .gitpod/blt.yml blt/local.blt.yml
+blt drupal:install -n
+apache2ctl start
